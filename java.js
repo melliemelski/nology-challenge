e33fc362af6be93a6c7c2ee5c85e9aff56b0021d
melliemelski <42639343+melliemelski@users.noreply.github.com>
2020-05-15 10:34:49 +0100

    Add files via upload
    Adding javascript

--- java.js
+++ java.js
@@ -10,3 +10,35 @@ function myFunction(imgs) {
     // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
   }
+
+
+    
+
+    function vote(){
+    let radios = document.getElementsByName("drink");
+    let choice = document.getElementById("result");
+    for (let i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+        //alert(radios[i].value);
+        choice.innerText = "Your choice is " + (radios[i].checked);
+      break;
+        }
+      }
+    }
+function changeCookie() {
+    if (document.getElementById("cookie").src = "images/cookie/cookie1.jpg") 
+    {
+      document.getElementById("cookie").src = "images/cookie/cookie2.jpg";
+    } else {
+      document.getElementById("cookie").src = "images/cookie/cookie1.jpg";
+    }
+}
+
+function fortune() {
+  let answers = ["With great listening comes great learning", "Rome wasn't built in a day", "A problem shared is a problem halved", "Think twice, code once", "You can't use up creativity"];
+  let fort = document.getElementById("fortunetext");
+           fort.innerText = "";
+           let num = Math.floor(Math.random() * (answers.length));
+           fort.innerText += answers[num];
+}
+
